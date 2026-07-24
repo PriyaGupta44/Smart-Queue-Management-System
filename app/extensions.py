a9a5aec91b4deb54c6cf572ee7966241bf492d85
Priya Gupta <priyatopriya3103@gmail.com>
2026-07-24 08:13:41 +0545

    feat(extensions): register Mail extension instance

--- app/extensions.py
+++ app/extensions.py
@@ -16,14 +16,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
 from flask_migrate import Migrate
+from flask_mail import Mail
 
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 csrf = CSRFProtect()
 migrate = Migrate()
+mail = Mail()
 
-# Where Flask-Login sends anonymous users who hit a @login_required route
 login_manager.login_view = "auth.login"
 login_manager.login_message = "Please log in to access this page."
 login_manager.login_message_category = "info"
